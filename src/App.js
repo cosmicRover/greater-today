7ea38a5d19238d57c50da7c84a069c1e786dca3d
joypaul <jpaul3250@gmail.com>
2020-05-22 19:34:42 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import FlareComponent from "flare-react";
+import check from './orbis.flr';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <FlareComponent
+      width={1750}
+      height={1500}
+      animationName="revolution"
+      file={check}
+    />
   );
 }
 
